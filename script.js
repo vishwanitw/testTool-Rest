07aac6d626c271504973b70d63ab9632dec8ceda
unknown <puviswan@DIN67000301.corp.capgemini.com>
2014-11-28 23:34:40 +0530

    added

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	// body...
+	alert("Hiiiiii");
+	// $('#tab-two').click(function  () {
+	// 	// body...
+
+
+	// 	var tab2 = document.getElementById('tab-two').text();
+$('#tab-one').click(function () {
+	 $('#two ,#three, #four').css("display", "none");
+	 $('#one').css("display" ,"block");
+	//$('#content').find('#one').remove();
+	//$('#one').css("display" ,"block");
+	//$('div').not('#one').remove();
+	//$('div:not(#one)').remove();
+	});
+
+});
+
+
+	
